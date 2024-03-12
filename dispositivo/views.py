cc8307ccbe50fd1e4276556a7bbcaf90b2bbaf32
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-03-12 19:59:20 -0300

    feat: criação de projeto e app api rest dispositivos

---
+++ dispositivo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Dispositivo
+from .serializers import DispositivoSerializer
+from rest_framework import generics
+
+class DispositivoList(generics.ListCreateAPIView):
+    queryset = Dispositivo.objects.all()
+    serializer_class = DispositivoSerializer
+    
+class DispositivoById(generics.ListAPIView):
+    queryset = Dispositivo.objects.all()
+    serializer_class = DispositivoByIdSerializer    
