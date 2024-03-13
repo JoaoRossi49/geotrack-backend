08d164caa5592035e4c04c9d05b2d64e66180beb
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-03-13 00:14:05 -0300

    feat: validação de código de dispositivo e inserção de coordenadas

--- dispositivo/mqtt_manager.py
+++ dispositivo/mqtt_manager.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import threading
 from django.conf import settings
+from datetime import datetime
 import json
 from .models import Dispositivo, Coordenada
 
@@ -13,23 +14,31 @@ class MQTTManager:
         self.client.on_message = self.on_message
 
         self.client.connect(settings.MQTT_BROKER, settings.MQTT_PORT, settings.MQTT_KEEP_ALIVE_INTERVAL)
-        self.client.subscribe(settings.MQTT_TOPIC, qos=1)
+        self.client.subscribe(settings.MQTT_TOPIC, qos=0)
 
     def on_message(self, client, userdata, msg):
-        payload = json.loads(msg.payload.decode('utf-8'))
-        codigo_dispositivo = payload.get('codigo_dispositivo')
-        latitude = payload.get('latitude')
-        longitude = payload.get('longitude')
-        if DispositivoManager.filter(codigo=codigo_dispositivo).exists():
-            coordenadas = Coordenada(
-                latitude=latitude,
-                longitude=longitude,
-            )
-            coordenadas.save()
-
+        try:
+            payload = msg.payload.decode('utf-8')
+            payload = json.loads(payload)
+            codigo_dispositivo = payload.get('codigo_dispositivo')
+            latitude = payload.get('latitude')
+            longitude = payload.get('longitude')
+            print('Recebeu payload')
+            if DispositivoManager.filter(codigo=codigo_dispositivo).exists():
+                print('Este celular existe!')
+                coordenadas = Coordenada(
+                    latitude=latitude,
+                    longitude=longitude,
+                    data_hora_coleta=datetime.now(),
+                )
+                try:
+                    coordenadas.save()
+                    print('Salvou no banco!')
+                except:
+                    print('não salvou no banco')
+        except:
+            print('Deu ruim, mas o payload é: ')
+            print(msg.payload) 
     def start(self):
         self.thread = threading.Thread(target=self.client.loop_forever)
         self.thread.start()
-
-mqtt_manager = MQTTManager()
-mqtt_manager.start()
