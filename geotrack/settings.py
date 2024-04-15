07617bf53e29d2c2a74089647aacf4a41de680bc
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-04-15 20:34:49 -0300

    feat: mudança de base de dados para postgresql

--- geotrack/settings.py
+++ geotrack/settings.py
@@ -92,8 +92,10 @@ WSGI_APPLICATION = 'geotrack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geotrack',
+        'ENGINE':'django.db.backends.postgresql',
+        'USER':'postgres',
+        'PASSWORD':'1234',
     }
 }
 
