f8a4af3ce02481e5ebcb3e515b6b097f8e6b3e8e
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-05-02 19:03:44 -0300

    feat: relacionamento dispositivo_coordenada

--- dispositivo/admin.py
+++ dispositivo/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Dispositivo
+from .models import Dispositivo, Coordenada
 
-admin.site.register(Dispositivo)
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'codigo', 'descricao')
+
+@admin.register(Coordenada)
+class CoordenadaAdmin(admin.ModelAdmin):
+    list_display = ('dispositivo_id', 'latitude', 'longitude', 'data_hora_inclusao', 'data_hora_coleta')
--- dispositivo/models.py
+++ dispositivo/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from .signals import send_database_update_message
-
+import uuid
 
 class Dispositivo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     codigo = models.CharField(max_length=200)
     descricao = models.CharField(max_length=500)
 
 class Coordenada(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dispositivo = models.ForeignKey(Dispositivo, default=uuid.uuid4, on_delete=models.CASCADE, blank=True, null=True)
     latitude = models.CharField(max_length=100, blank=True)
     longitude = models.CharField(max_length=100, blank=True)
-    data_hora_coleta = models.DateTimeField()
+    data_hora_inclusao = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    data_hora_coleta = models.DateTimeField(blank=True, null=True)
     def save(self, *args, **kwargs):
         send_database_update_message(True, self.latitude, self.longitude)
-        if self.data_hora_coleta:
-            self.data_hora_coleta = timezone.make_aware(self.data_hora_coleta)
         super().save(*args, **kwargs)      
