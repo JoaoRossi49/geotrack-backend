89800c38b37cc97ceacd3a22b34119f318e470f8
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-05-08 21:39:35 -0300

    feat: coordenada é relacionda a veículo, alteração de nome de pin em mapa

--- dispositivo/amqp_manager.py
+++ dispositivo/amqp_manager.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from datetime import datetime
 import json, pika
 import uuid
-from .models import Dispositivo, Coordenada
+from .models import Dispositivo, Coordenada, Veiculo
 
 DispositivoManager = Dispositivo.objects
 
@@ -29,12 +29,14 @@ class AMQPManager:
                     payload = body.decode('utf-8')
                     payload = json.loads(payload)
                     dispositivo_id = payload.get('dispositivo_id')
+                    veiculo_id = payload.get('veiculo_id')
                     latitude = payload.get('latitude')
                     longitude = payload.get('longitude')                    
                     if DispositivoManager.filter(id=dispositivo_id).exists():
                         coordenadas = Coordenada(
                             id=uuid.uuid4(),
                             dispositivo = Dispositivo.objects.get(id=dispositivo_id),
+                            veiculo = Veiculo.objects.get(id=veiculo_id),
                             latitude=latitude,
                             longitude=longitude,
                             data_hora_coleta=datetime.now(),
